280afa50e2dff2544498c49f9cd2fa768ea4b8b8
metanas <com.com404@gmail.com>
2018-08-02 23:56:19 +0100

    fix login

--- catalog/controller/extension/total/coupon.php
+++ catalog/controller/extension/total/coupon.php
@@ -40,7 +40,7 @@ class ControllerExtensionTotalCoupon extends Controller {
 
 			$this->session->data['success'] = $this->language->get('text_success');
             $json['coupon'] = $coupon_info;
-			$json['redirect'] = $this->url->link('checkout/cart', 'language=' . $this->config->get('config_language'));
+//			$json['redirect'] = $this->url->link('checkout/cart', 'language=' . $this->config->get('config_language'));
 		} else {
 			$json['error'] = $this->language->get('error_coupon');
 		}
