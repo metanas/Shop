89ba871b32b006048ed663af7af91bdfb897ea91
metanas <com.com404@gmail.com>
2018-11-29 20:48:51 +0100

    fix js

--- catalog/language/fr-Fr/checkout/success.php
+++ catalog/language/fr-Fr/checkout/success.php
@@ -2,13 +2,14 @@
 
 // Heading
 
-$_['heading_title']        ='Votre commande a bien été reçue!';
+$_['heading_title'] = 'Thank you!';
 
 
 // Text
 
-$_['text_basket']          ='Mon panier';
-$_['text_checkout']        ='Check-out';
-$_['text_success']         ='Succès';
-$_['text_customer']        =' <p style="font-size: 22px;" class=" col-md-12 text-center"> Thank you!</p>    <p class=" col-md-12 text-center" style="margin-left: -2px;"> Vous étes sur le point de recevoir vos nouvelles paires de chaussures. </p>  <p class=" col-md-12 text-center" style="margin-left: -5px;"> L\'équipe de zalark est entrain de traiter votre comande est la préparer </p><p class=" col-md-12 text-center" style="margin-left: -173px;"> pour livraison.</p> </p>  <p class="col-md-12 text-center"> Vous recevez un email de confirmation, et vous pourriez a tout moment </p> <p class="col-md-12 text-center" style="margin-left: 6px;"> suivre votre commande en cliquant sur mes commandes sur Zalark.com </p>  <p class="col-md-12 text-center" style="margin-left: 17px;">Nous vous remercions encore une fois pour votre achat chez Zalark.com , en </p>  <p class=" col-md-12 text-center" style="margin-left: -125px;">  esperant vous voir trés bientôt </p> ';
-$_['text_guest']           ='  <p> Votre commande a été traitée avec succès! </p>  <p> Veuillez adresser vos questions à la <a href="%s"> propriétaire du magasin </a> . </p>  <p> Merci d`avoir acheté avec nous en ligne! </p> ';
+$_['text_basket'] = 'Mon panier';
+$_['text_checkout'] = 'Check-out';
+$_['text_success'] = 'Succès';
+$_['text_customer'] = 'Salut %s,';
+$_['text_message'] = '<p style="line-height: 30px;font-size: 18px">Vous étes sur le point de recevoir vos nouvelles paires de chaussures. L\'équipe de zalark est entrain de traiter votre comande est la préparer pour livraison.</p><p style="line-height: 30px;font-size: 18px">Vous recevez un email de confirmation, et vous pourriez a tout moment suivre votre commande en cliquant sur <a href="%s" style="color: dodgerblue">mes commandes</a> sur Zalark.com</p> <p style="line-height: 30px;font-size: 18px">Nous vous remercions encore une fois pour votre achat chez Zalark.com , en esperant vous voir trés bientôt. </p> ';
+$_['text_guest'] = '  <p> Votre commande a été traitée avec succès! </p>  <p> Veuillez adresser vos questions à la <a href="%s"> propriétaire du magasin </a> . </p>  <p> Merci d`avoir acheté avec nous en ligne! </p> ';
