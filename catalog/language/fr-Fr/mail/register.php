27eac14f50df7889e80aefb3b78b89a76bc3fce6
metanas <com.com404@gmail.com>
2018-12-13 18:39:50 +0100

    fix register mail

--- catalog/language/fr-Fr/mail/register.php
+++ catalog/language/fr-Fr/mail/register.php
@@ -1,15 +1,15 @@
 <?php
 // Text
-$_['text_subject']         = '%s - Merci pour votre inscription';
-$_['text_welcome']         = 'Bienvenue et merci pour votre inscription chez %s!';
-$_['text_login']           = 'Votre compte a été créé et vous pouvez vous connecter en utilisant votre adresse e-mail et votre mot de passe en visitant notre site Web ou à l`URL suivante:';
-$_['text_approval']        = 'Votre compte doit étre approuvé avant de pouvoir vous connecter. en visitant ce lien en premier: %s ,Une fois approuvé, vous pouvez vous connecter en utilisant votre adresse e-mail et votre mot de passe en visitant notre site Web ou à l`URL suivante:';
-$_['text_service']         = 'Lors de la connexion, vous pourrez accéder à d`autres services, y compris l`examen des commandes passées, l`impression de factures et l`édition des informations de votre compte.';
-$_['text_thanks']          = 'Merci,';
-$_['text_new_customer']    = 'Nouveau client';
-$_['text_signup']          = 'Un nouveau client s`est inscrit:';
+$_['text_subject'] = '%s - Merci pour votre inscription';
+$_['text_welcome'] = 'Bienvenue et merci pour votre inscription chez %s!';
+$_['text_login'] = 'Votre compte a été créé et vous pouvez vous connecter en utilisant votre adresse e-mail et votre mot de passe en visitant notre site Web ou à l`URL suivante:';
+$_['text_approval'] = '<p>Bonjour %s! Merci de cliquer sur le lien ci-dessus pour valider votre compte:<br/> %s</p><br/> <p>Avex votre compte, vous pouvez choisir vos artcles preféres pour votre prochains achats, créer votre agenda d\'adresse et veaucoup plus de chose.';
+$_['text_service'] = 'Lors de la connexion, vous pourrez accéder à d`autres services, y compris l`examen des commandes passées, l`impression de factures et l`édition des informations de votre compte.';
+$_['text_thanks'] = 'Cordialement,';
+$_['text_new_customer'] = 'Nouveau client';
+$_['text_signup'] = 'Un nouveau client s`est inscrit:';
 $_['entry_customer_group'] = 'Groupe de clients:';
-$_['entry_firstname']      = 'Votre Prénom:';
-$_['entry_lastname']       = 'Nom de famille:';
-$_['entry_email']          = 'Email';
-$_['entry_telephone']      = 'Téléphone';
+$_['entry_firstname'] = 'Votre Prénom:';
+$_['entry_lastname'] = 'Nom de famille:';
+$_['entry_email'] = 'Email';
+$_['entry_telephone'] = 'Téléphone';
