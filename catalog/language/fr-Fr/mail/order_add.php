6a4e1f35dfafdb8cf3e1138399d82887de89f3b3
metanas <com.com404@gmail.com>
2018-12-21 20:47:13 +0100

    fix order email

--- catalog/language/fr-Fr/mail/order_add.php
+++ catalog/language/fr-Fr/mail/order_add.php
@@ -1,28 +1,31 @@
 <?php
 // Text
 $_['text_subject']          = '%s - Ordonnance %s';
-$_['text_greeting']         = 'Votre commande chez %s a �t� re�ue avec succ�s et sera trait�e une fois le paiement confirm�.';
+$_['text_user']             = 'Salut %s,';
+$_['text_thanks']           = 'Merci pour votre commande!';
+$_['text_missing']          = 'Nous espérons vous voir trés bientot';
+$_['text_greeting']         = 'Votre commande chez %s a été reçue avec succés et sera traitée une fois le paiement confirmé.';
 $_['text_link']             = 'Pour afficher votre commande, cliquez sur le lien ci-dessous:';
 $_['text_order_detail']     = 'Détails de la commande';
 $_['text_instruction']      = 'Instructions';
-$_['text_order_id']         = 'Num�ro de commande:';
-$_['text_date_added']       == 'Date ajout�e:';
+$_['text_order_id']         = 'Numéro de commande:';
+$_['text_date_added']       = 'Date de la commande:';
 $_['text_order_status']     = 'Statut de la commande:';
 $_['text_payment_method']   = 'Mode de paiement:';
-$_['text_shipping_method']  == 'M�thode d`envoi:';
+$_['text_shipping_method']  = 'Méthode d`envoi:';
 $_['text_email']            = 'E-mail:';
-$_['text_telephone']        = 'T�l�phone:';
+$_['text_telephone']        = 'Téléphone:';
 $_['text_ip']               = 'Adresse IP:';
 $_['text_payment_address']  = 'Adresse de facturation';
 $_['text_shipping_address'] = 'Adresse de livraison';
 $_['text_products']         = 'Des produits';
 $_['text_product']          = 'Produit';
 $_['text_model']            = 'maquette';
-$_['text_quantity']         = 'Quantit�';
+$_['text_quantity']         = 'Quantité';
 $_['text_price']            = 'Prix';
 $_['text_order_total']      = 'Totaux de commande';
 $_['text_total']            = 'Total';
-$_['text_download']         = 'Une fois votre paiement confirm�, vous pouvez cliquer sur le lien ci-dessous pour acc�der � vos produits t�l�chargeables:';
+$_['text_download']         = 'Une fois votre paiement confirmé, vous pouvez cliquer sur le lien ci-dessous pour accéder á vos produits téléchargeables:';
 $_['text_comment']          = 'Les commentaires pour votre commande sont les suivants:';
-$_['text_footer']           = 'Veuillez r�pondre � cet e-mail si vous avez des questions.';
+$_['text_footer']           = 'Veuillez répondre á cet e-mail si vous avez des questions.';
 
