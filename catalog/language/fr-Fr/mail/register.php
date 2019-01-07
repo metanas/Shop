9a904c0a49d5fc3cdff515d4fd7941cd96471cac
metanas <metanas@pre-history.com>
2019-01-07 22:57:06 +0100

    add express shipping

--- catalog/language/fr-Fr/mail/register.php
+++ catalog/language/fr-Fr/mail/register.php
@@ -3,7 +3,7 @@
 $_['text_subject'] = '%s - Merci pour votre inscription';
 $_['text_welcome'] = 'Bienvenue et merci pour votre inscription chez %s!';
 $_['text_login'] = 'Votre compte a été créé et vous pouvez vous connecter en utilisant votre adresse e-mail et votre mot de passe en visitant notre site Web ou à l`URL suivante:';
-$_['text_approval'] = '<p style="text-align: justify;font-size:20px ">Bonjour <span style="text-transform: uppercase;">%s</span>! Merci de cliquer sur le lien ci-dessus pour valider votre compte:<br/> %s</p><br/> <p style="text-align: justify;font-size:20px ">Avec votre compte, vous pouvez choisir vos articles preférés pour votre prochain achats, créer votre agenda d\'adresse et beaucoup plus de choses.';
+$_['text_approval'] = '<p style="text-align: justify;font-size:20px ">Bonjour <span style="text-transform: uppercase;">%s</span>! Merci de cliquer sur le lien ci-dessus pour valider votre compte:<br/> <a href="%s">%s</a></p><br/> <p style="text-align: justify;font-size:20px ">Avec votre compte, vous pouvez choisir vos articles preférés pour votre prochain achats, créer votre agenda d\'adresse et beaucoup plus de choses.';
 $_['text_service'] = 'Lors de la connexion, vous pourrez accéder à d`autres services, y compris l`examen des commandes passées, l`impression de factures et l`édition des informations de votre compte.';
 $_['text_thanks'] = 'Cordialement,';
 $_['text_new_customer'] = 'Nouveau client';
