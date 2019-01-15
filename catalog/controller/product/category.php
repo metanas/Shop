110503bb4ce6b468314f0e820b714d6f6a9d5662
metanas <com.com404@gmail.com>
2019-01-15 08:45:12 +0000

    fix similar product

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -769,7 +769,7 @@ class ControllerProductCategory extends Controller
             $this->model_catalog_product->getProduct($result);
 
             if ($result['image']) {
-                $image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+                $image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_popup_height'));
             } else {
                 $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
             }
