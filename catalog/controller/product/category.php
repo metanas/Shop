87557735d732a6d8103bf498665d1da1431191b0
zahrahrach <zahrahrach@gmail.com>
2019-02-01 13:01:21 +0100

    fix

--- catalog/controller/product/category.php
+++ catalog/controller/product/category.php
@@ -142,7 +142,9 @@ class ControllerProductCategory extends Controller
             if (isset($this->request->get['price-max'])) {
                 $url .= '&price-max=' . $this->request->get['price-max'];
             }
-
+            if (isset($this->request->get['special'])) {
+                $filter['special'] = $this->request->get['special'];
+            }
             $data['categories'] = array();
 
             $results = $this->model_catalog_category->getCategories($category_id);
@@ -288,6 +290,9 @@ class ControllerProductCategory extends Controller
 
             if (isset($this->request->get['price-max'])) {
                 $url .= '&price-max=' . $this->request->get['price-max'];
+            }
+            if (isset($this->request->get['special'])) {
+                $filter['special'] = $this->request->get['special'];
             }
 
             $data['sorts'] = array();
@@ -583,8 +588,10 @@ class ControllerProductCategory extends Controller
 
                 if ((float)$result['special']) {
                     $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+                    $discount = intval((($result['price'] - $result['special']) * 100) / $result['price']);
                 } else {
                     $special = false;
+                    $discount = false;
                 }
 
                 if ($this->customer->isLogged()) {
@@ -613,6 +620,7 @@ class ControllerProductCategory extends Controller
                     'manufacturer' => $result['manufacturer'],
                     'name' => (strlen($result['name']) <= 12) ? $result['name'] : utf8_substr(trim(strip_tags(html_entity_decode($result['name'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
                     'price' => $price,
+                    'discount'=> $discount,
                     'stock' => $stock,
                     'special' => $special,
                     'favorite' => $favorite,
@@ -642,6 +650,10 @@ class ControllerProductCategory extends Controller
 
             if (isset($this->request->get['price-min'])) {
                 $url .= '&price-min=' . $this->request->get['price-min'];
+            }
+
+            if (isset($this->request->get['special'])) {
+                $filter['special'] = $this->request->get['special'];
             }
 
             if (isset($this->request->get['price-max'])) {
@@ -692,6 +704,10 @@ class ControllerProductCategory extends Controller
 
             if (isset($this->request->get['size'])) {
                 $url .= '&size=' . $this->request->get['size'];
+            }
+
+            if (isset($this->request->get['special'])) {
+                $filter['special'] = $this->request->get['special'];
             }
 
             if (isset($this->request->get['price-min'])) {
